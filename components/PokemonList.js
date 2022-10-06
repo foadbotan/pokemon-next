7e9da33a11cb526f9ad65d1ccd5425a81660785e
Foad Botan <foadbotan@gmail.com>
2022-10-07 01:59:56 +1100

    Moved pokemon fetch to Home and added pagination

--- components/PokemonList.js
+++ components/PokemonList.js
@@ -1,17 +1,6 @@
-import { useEffect, useState } from "react";
 import Pokemon from "./Pokemon";
 
-const URL = "https://pokeapi.co/api/v2/pokemon/?offset=0&limit=20";
-
-export default function PokemonList() {
-  const [pokemonList, setPokemonList] = useState([]);
-
-  useEffect(() => {
-    fetch(URL)
-      .then((res) => res.json())
-      .then((data) => setPokemonList(data.results));
-  }, []);
-
+export default function PokemonList({ pokemonList }) {
   return (
     <div>
       {pokemonList.map((pokemon) => (
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,40 @@
+import { useState, useEffect } from "react";
 import Head from "next/head";
 import PokemonList from "../components/PokemonList";
 
+const TOTAL_NUMBER_OF_POKEMON = 1154;
+const POKEMON_PER_PAGE = 20;
+const TOTAL_NUMBER_OF_PAGES = Math.floor(TOTAL_NUMBER_OF_POKEMON / POKEMON_PER_PAGE);
+const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
+
 export default function Home() {
+  const [pokemonList, setPokemonList] = useState([]);
+  const [page, setPage] = useState(0);
+  const [nextPage, setNextPage] = useState("");
+  const [previousPage, setPreviousPage] = useState("");
+
+  useEffect(() => {
+    const URL = `${BASE_URL}?offset=${page * POKEMON_PER_PAGE}&limit=${POKEMON_PER_PAGE}`;
+
+    fetch(URL)
+      .then((res) => res.json())
+      .then((data) => {
+        setPokemonList(data.results);
+        setNextPage(data.next);
+        setPreviousPage(data.previous);
+      });
+  }, [page]);
+
+  function handlePreviousPage() {
+    if (page === 0) return;
+    setPage((prevPage) => prevPage - 1);
+  }
+
+  function handleNextPage() {
+    if (nextPage === null) return;
+    setPage((prevPage) => prevPage + 1);
+  }
+
   return (
     <div>
       <Head>
@@ -10,7 +43,26 @@ export default function Home() {
 
       <main>
         <h1 className="text-5xl text-center m-5">Pokedex</h1>
-        <PokemonList />
+        <div className="space-x-3 flex justify-center items-center">
+          <button
+            className="py-2 px-3 rounded bg-green-600 disabled:bg-gray-200"
+            onClick={handlePreviousPage}
+            disabled={previousPage === null}
+          >
+            &lt;
+          </button>
+          <p>
+            Page {page + 1} of {TOTAL_NUMBER_OF_PAGES + 1}
+          </p>
+          <button
+            className="py-2 px-3 rounded bg-green-600 disabled:bg-gray-200"
+            onClick={handleNextPage}
+            disabled={nextPage === null}
+          >
+            &gt;
+          </button>
+        </div>
+        <PokemonList pokemonList={pokemonList} />
       </main>
     </div>
   );
