dfd2500d9dbb25aabd2adfc3cc436d668a7b46d6
Foad Botan <foadbotan@gmail.com>
2022-10-07 02:38:58 +1100

    Styled Pagination

--- components/Pagination.js
+++ components/Pagination.js
@@ -10,24 +10,27 @@ export default function Pagination({
   page,
 }) {
   return (
-    <div className="space-x-3 flex justify-center items-center">
-      <button
-        className="py-2 px-3 rounded bg-green-600 disabled:bg-gray-200"
-        onClick={handlePreviousPage}
-        disabled={previousPage === null}
-      >
-        &lt;
-      </button>
-      <p>
-        Page {page + 1} of {NUMBER_OF_PAGES + 1}
-      </p>
-      <button
-        className="py-2 px-3 rounded bg-green-600 disabled:bg-gray-200"
-        onClick={handleNextPage}
-        disabled={nextPage === null}
-      >
-        &gt;
-      </button>
+    <div className="grid place-content-center">
+      <div className="bg-white py-2 px-5 rounded-md shadow space-x-3 flex items-center">
+        <button
+          className="py-1 px-3 rounded bg-green-500 disabled:bg-gray-200"
+          onClick={handlePreviousPage}
+          disabled={previousPage === null}
+        >
+          &lt;
+        </button>
+        <button>
+          Page {page + 1} of {NUMBER_OF_PAGES}
+        </button>
+
+        <button
+          className="py-1 px-3 rounded bg-green-500 disabled:bg-gray-200"
+          onClick={handleNextPage}
+          disabled={nextPage === null}
+        >
+          &gt;
+        </button>
+      </div>
     </div>
   );
 }
