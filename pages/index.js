f5923189add8fb15da8fd365568817aecd05cd2f
Foad Botan <foadbotan@gmail.com>
2022-10-08 22:18:39 +1100

    Moved fetch to a func

--- pages/index.js
+++ pages/index.js
@@ -11,23 +11,27 @@ export default function Home() {
   const [previousPage, setPreviousPage] = useState("");
 
   useEffect(() => {
-    fetch(URL)
+    fetchPokemon(URL);
+  }, []);
+
+  function handlePreviousPage() {
+    if (previousPage === null) return;
+    fetchPokemon(previousPage);
+  }
+
+  function handleNextPage() {
+    if (nextPage === null) return;
+    fetchPokemon(nextPage);
+  }
+
+  function fetchPokemon(url) {
+    fetch(url)
       .then((res) => res.json())
       .then((data) => {
         setPokemonList(data.results);
         setNextPage(data.next);
         setPreviousPage(data.previous);
       });
-  }, []);
-
-  function handlePreviousPage() {
-    if (page === 0) return;
-    setPage((prevPage) => prevPage - 1);
-  }
-
-  function handleNextPage() {
-    if (nextPage === null) return;
-    setPage((prevPage) => prevPage + 1);
   }
 
   return (
@@ -38,6 +42,12 @@ export default function Home() {
 
       <main>
         <h1 className="m-5 text-center text-5xl">Pokedex</h1>
+        <button onClick={handlePreviousPage} className="m-3 bg-gray-200 p-4">
+          &#60;
+        </button>
+        <button onClick={handleNextPage} className="m-3 bg-gray-200 p-4">
+          &#62;
+        </button>
         <PokemonList pokemonList={pokemonList} />
       </main>
     </div>
