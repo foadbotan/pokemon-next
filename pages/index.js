8ffbcbd7416a40a22b2e3150bbd9aff549ac4556
Foad Botan <foadbotan@gmail.com>
2022-10-14 14:34:00 +1100

    Minor renaming and refactoring

--- pages/index.js
+++ pages/index.js
@@ -19,7 +19,7 @@ export default function Home({ allPokemon }) {
   const [search, setSearch] = useState("");
   const [filteredPokemon, setFilteredPokemon] = useState([]);
   const [pokemonList, setPokemonList] = useState([]);
-  const ref = useRef(null);
+  const loadMoreRef = useRef();
 
   useEffect(() => {
     const includesSearchString = (pokemon) => new RegExp(search, "i").test(pokemon.name);
@@ -28,32 +28,24 @@ export default function Home({ allPokemon }) {
   }, [search, allPokemon]);
 
   useEffect(() => {
-    const observer = new IntersectionObserver(
-      ([entry]) => {
-        if (entry.isIntersecting) {
-          setPokemonList((prevPokemonList) => {
-            const nextPokemon = filteredPokemon.slice(
-              prevPokemonList.length,
-              prevPokemonList.length + 10
-            );
-            return [...prevPokemonList, ...nextPokemon];
-          });
-        }
-      },
-      {
-        root: null,
-        rootMargin: "0px",
-        threshold: 1.0,
+    const observer = new IntersectionObserver(([entry]) => {
+      if (entry.isIntersecting) {
+        setPokemonList((prev) => [
+          ...prev,
+          ...filteredPokemon.slice(prev.length, prev.length + 10),
+        ]);
       }
-    );
+    });
 
-    const currentRef = ref.current;
+    const currentRef = loadMoreRef.current;
     if (currentRef) observer.observe(currentRef);
 
     return () => {
-      if (currentRef) observer.unobserve(currentRef);
+      if (currentRef) {
+        observer.unobserve(currentRef);
+      }
     };
-  }, [filteredPokemon, pokemonList]);
+  }, [filteredPokemon]);
 
   return (
     <div>
@@ -70,12 +62,12 @@ export default function Home({ allPokemon }) {
           placeholder="Search "
         />
         <div className="flex flex-wrap justify-center gap-10">
-          {pokemonList.map((pokemon, index) => {
-            return <Pokemon key={pokemon.name} {...pokemon} />;
-          })}
+          {pokemonList.map((pokemon, index) => (
+            <Pokemon key={pokemon.name} {...pokemon} />
+          ))}
         </div>
 
-        <div ref={ref}></div>
+        <div ref={loadMoreRef}></div>
       </main>
     </div>
   );
