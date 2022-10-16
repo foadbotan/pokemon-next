ceff2803a07ea6570671d336bd7c806fa8315a5f
Foad Botan <foadbotan@gmail.com>
2022-10-16 14:17:33 +1100

    Created individual pokemon pages

---
+++ pages/[pokemon].js
@@ -0,0 +1,33 @@
+import { useRouter } from "next/router";
+import Link from "next/link";
+
+const URL = "https://pokeapi.co/api/v2/pokemon/";
+
+export default function Pokemon({ pokemonData }) {
+  console.log(pokemonData.name);
+
+  return (
+    <div>
+      <Link href="/">⬅ Back to Pokemon</Link>
+      <h1 className="m-5 text-center text-5xl capitalize">{pokemonData.name}</h1>
+    </div>
+  );
+}
+
+export async function getStaticPaths() {
+  const res = await fetch(URL);
+  const { results } = await res.json();
+
+  const paths = results.map(({ name }) => ({
+    params: { pokemon: name },
+  }));
+
+  return { paths, fallback: true };
+}
+
+export async function getStaticProps({ params }) {
+  const res = await fetch(`${URL}${params.pokemon}`);
+  const pokemonData = await res.json();
+
+  return { props: { pokemonData } };
+}
