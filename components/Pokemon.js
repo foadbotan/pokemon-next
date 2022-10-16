6da8cb95d321ead23763386b9bcf4cfd9a0b8c5e
Foad Botan <foadbotan@gmail.com>
2022-10-16 14:17:53 +1100

    Linked cards to their pokemon page

--- components/Pokemon.js
+++ components/Pokemon.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Pokemon({ url, name }) {
   const [loading, setLoading] = useState(true);
@@ -36,27 +37,31 @@ export default function Pokemon({ url, name }) {
   if (loading || !image || !abilities || !types || !color) return null;
 
   return (
-    <div className="relative flex w-52 cursor-pointer flex-col justify-between overflow-hidden rounded-lg shadow-lg">
-      <div
-        className="absolute -z-20 h-full w-full opacity-30 saturate-50"
-        style={{ backgroundColor: color }}
-      ></div>
-      <h2 className="-mb-5 px-2 pt-5 text-center text-3xl font-bold capitalize">{name}</h2>
-      <div className="relative flex aspect-square items-center justify-center">
-        <div className="absolute h-3/5 w-full bg-white"></div>
-        <Image src={image} alt={name} height="200px" width="200px" />
+    <Link href={`/${name}`}>
+      <div className="relative flex w-52 cursor-pointer flex-col justify-between overflow-hidden rounded-lg shadow-lg">
+        <div
+          className="absolute -z-20 h-full w-full opacity-30 saturate-50"
+          style={{ backgroundColor: color }}
+        ></div>
+        <h2 className="-mb-5 px-2 pt-5 text-center text-3xl font-bold capitalize">{name}</h2>
+        <div className="relative flex aspect-square items-center justify-center">
+          <div className="absolute h-3/5 w-full bg-white"></div>
+          <Image src={image} alt={name} height="200px" width="200px" />
+        </div>
+        <div className="-mt-5 p-3">
+          <p className="text-xs tracking-widest opacity-50">Abilities:</p>
+          <p className="capitalize">{abilities.join(", ")}</p>
+        </div>
+        <div className="-mt-5 p-3">
+          <p className="text-xs tracking-widest opacity-50">Type:</p>
+          <p className="capitalize">
+            {types.join(", ")}
+            <span className=" float-right rounded border bg-white py-0.5 px-2 opacity-50">
+              {id}
+            </span>
+          </p>
+        </div>
       </div>
-      <div className="-mt-5 p-3">
-        <p className="text-xs tracking-widest opacity-50">Abilities:</p>
-        <p className="capitalize">{abilities.join(", ")}</p>
-      </div>
-      <div className="-mt-5 p-3">
-        <p className="text-xs tracking-widest opacity-50">Type:</p>
-        <p className="capitalize">
-          {types.join(", ")}
-          <span className=" float-right rounded border bg-white py-0.5 px-2 opacity-50">{id}</span>
-        </p>
-      </div>
-    </div>
+    </Link>
   );
 }
