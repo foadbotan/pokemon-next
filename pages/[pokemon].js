66635874213b3e164ff1eb18c9e9688d55d9ff12
Foad Botan <foadbotan@gmail.com>
2022-10-18 20:01:47 +1100

    Restart Pokemon page from scratch

--- pages/[pokemon].js
+++ pages/[pokemon].js
@@ -1,166 +1,59 @@
-import Link from "next/link";
 import Image from "next/image";
-import PokemonCard from "../components/PokemonCard";
+import Layout from "../components/Layout";
 
-const POKEMON_URL = "https://pokeapi.co/api/v2/pokemon/";
+const URL = "https://pokeapi.co/api/v2/pokemon/";
 
-export default function Pokemon({ pokemonData, evolutions, varietiesData, speciesData, error }) {
-  if (error)
-    return (
-      <div>
-        <Link href="/">
-          <p className="m-5 cursor-pointer text-xl underline">← Return to Pokemon search</p>
-        </Link>
-        <p className="m-5 text-2xl text-red-600">{error}</p>
-      </div>
-    );
-  if (!pokemonData) return <h1 className="m-20 text-center text-5xl">Loadding...</h1>;
-
-  const name = pokemonData.name;
-  const abilities = pokemonData.abilities.map(({ ability }) => ability.name);
-  const types = pokemonData.types.map(({ type }) => type.name);
-  const image = pokemonData.sprites.other["official-artwork"].front_default;
-  const moves = pokemonData.moves.map(({ move }) => move.name);
-  const stats = pokemonData.stats.map(({ base_stat, stat }) => ({
-    name: stat.name,
-    value: base_stat,
-  }));
-  const varieties = varietiesData.filter(
-    (variety) => !evolutions.some((evolution) => evolution.name === variety.name)
-  );
-  const color = speciesData.color.name;
+export default function Pokemon(props) {
+  const { color, name, abilities, types, image, moves, id, stats, japaneseName } = props;
 
   return (
-    <div className="">
-      <Link href="/">
-        <p className="m-5 cursor-pointer text-xl underline">← Pokemon search</p>
-      </Link>
-      <h1 className="m-5 text-center text-6xl capitalize">{name}</h1>
-      <div>
-        <div className="flex items-center">
-          <Image src={image} alt={name} height="400" width="400" />
-          <div className="flex gap-10">
-            <div>
-              <h2 className="text-4xl">Stats</h2>
-              <ul>
-                {stats.map(({ name, value }) => (
-                  <li key={name}>
-                    {name}: {value}
-                  </li>
-                ))}
-              </ul>
-            </div>
-            <div>
-              <h2 className="text-4xl">Type</h2>
-              <ul>
-                {types.map((type) => (
-                  <li key={type}>{type}</li>
-                ))}
-              </ul>
-            </div>
-            <div>
-              <h2 className="text-4xl">Abilities</h2>
-              <ul>
-                {abilities.map((ability) => (
-                  <li key={ability}>{ability}</li>
-                ))}
-              </ul>
-            </div>
-          </div>
-        </div>
-
-        <div className="m-5">
-          <h2 className="m-10 text-center text-4xl">Evolutions</h2>
-          <ul className="flex flex-wrap justify-center gap-10">
-            {evolutions.map((evolution) => (
-              <li key={evolution.name}>
-                <PokemonCard
-                  key={evolution.name}
-                  url={POKEMON_URL + evolution.name}
-                  name={evolution.name}
-                />
-              </li>
-            ))}
-          </ul>
-        </div>
-
-        {varieties.length > 0 && (
-          <div className="m-5">
-            <h2 className="m-10 text-center text-4xl">Varieties</h2>
-            <ul className="flex flex-wrap justify-center gap-10">
-              {varieties.map((pokemon) => {
-                return (
-                  <li key={pokemon.name}>
-                    <PokemonCard key={pokemon.name} {...pokemon} />
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
-
-        {moves.length > 0 && (
-          <div className="m-5 capitalize">
-            <h2 className="m-10 text-center text-4xl">Moves</h2>
-            <ul className="flex flex-wrap">
-              {moves.map((move) => (
-                <li key={move}>
-                  <p className="m-2 bg-white py-1 px-3 ">{move}</p>
-                </li>
-              ))}
-            </ul>
-          </div>
-        )}
+    <Layout title={name}>
+      <p className="absolute text-[12rem] font-black opacity-10">{japaneseName}</p>
+      <div className="flex">
+        <h2 className="text-6xl font-bold capitalize">{name}</h2>
+        <Image src={image} alt={name} height="300" width="300" />
       </div>
-    </div>
+      <div className="flex flex-wrap gap-4">
+        {types.map((type) => (
+          <p key={type} className="rounded-full px-4 py-2 text-sm font-bold capitalize">
+            {type}
+          </p>
+        ))}
+      </div>
+    </Layout>
   );
 }
 
 export async function getStaticPaths() {
-  const data = await fetch(POKEMON_URL).then((res) => res.json());
+  const data = await fetch(URL + "?limit=-1").then((res) => res.json());
+  const paths = data.results.map((pokemon) => ({
+    params: { pokemon: pokemon.name },
+  }));
 
-  const paths = data.results.map(({ name }) => ({ params: { pokemon: name } }));
-  return { paths, fallback: true };
+  return {
+    paths,
+    fallback: false,
+  };
 }
 
 export async function getStaticProps({ params: { pokemon } }) {
-  try {
-    const pokemonData = await fetch(POKEMON_URL + pokemon).then((res) => res.json());
-    const speciesData = await fetch(pokemonData.species.url).then((res) => res.json());
-    const varietiesData = speciesData.varieties.map(({ pokemon }) => pokemon);
-    const evolutionChainData = await fetch(speciesData.evolution_chain.url).then((res) =>
-      res.json()
-    );
-    const evolutions = getEvolutions(evolutionChainData.chain);
+  const pokemonData = await fetch(URL + pokemon).then((res) => res.json());
+  const speciesData = await fetch(pokemonData.species.url).then((res) => res.json());
 
-    function getEvolutions(chain) {
-      const evolutions = [];
-      let current = chain;
-      while (current) {
-        evolutions.push(current.species);
-        current = current.evolves_to[0];
-      }
-      return evolutions;
-    }
-
-    return {
-      props: {
-        pokemonData,
-        varietiesData,
-        evolutions,
-        speciesData,
-      },
-    };
-  } catch (error) {
-    console.log(error);
-  }
   return {
     props: {
-      pokemonData: null,
-      varietiesData: null,
-      evolutions: null,
-      speciesData: null,
-      error: `Error: Pokemon "${pokemon}" not found`,
+      japaneseName: speciesData.names.find((name) => name.language.name === "ja").name,
+      color: speciesData.color.name,
+      name: pokemonData.name,
+      abilities: pokemonData.abilities.map(({ ability }) => ability.name),
+      types: pokemonData.types.map(({ type }) => type.name),
+      image: pokemonData.sprites.other["official-artwork"].front_default,
+      moves: pokemonData.moves.map(({ move }) => move.name),
+      id: pokemonData.id.toString(),
+      stats: pokemonData.stats.map(({ base_stat, stat }) => ({
+        base_stat,
+        name: stat.name,
+      })),
     },
   };
 }
