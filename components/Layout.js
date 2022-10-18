1a62d98f79f7deb67b8a6e5240d0f488cde5708d
Foad Botan <foadbotan@gmail.com>
2022-10-18 13:12:31 +1100

    Created Layout comp

---
+++ components/Layout.js
@@ -0,0 +1,30 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Layout({ children, title, className = "" }) {
+  return (
+    <div>
+      <Head>
+        <title>{title}</title>
+      </Head>
+
+      <main className={`container mx-auto ${className}`}>
+        <div className="flex items-center justify-center">
+          <Link href="/">
+            <div>
+              <Image
+                src="/pokemon-logo.svg"
+                width="200"
+                height="100"
+                alt="Pokemon logo"
+                className=" cursor-pointer"
+              />
+            </div>
+          </Link>
+        </div>
+        {children}
+      </main>
+    </div>
+  );
+}
