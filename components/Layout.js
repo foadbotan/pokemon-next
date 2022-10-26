0f421042b2c60002028c9c113bc74478235c0ad2
Foad Botan <foadbotan@gmail.com>
2022-10-26 12:15:52 +1100

    Layout passes className and style to parent div

--- components/Layout.js
+++ components/Layout.js
@@ -2,14 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 
-export default function Layout({ children, title, className = "" }) {
+export default function Layout({ children, title, className = "", style = {} }) {
   return (
-    <div>
+    <div style={style} className={className}>
       <Head>
         <title>{title}</title>
       </Head>
 
-      <main className={`container mx-auto ${className}`}>
+      <main className="container mx-auto">
         <div className="mb-10 flex items-center justify-center">
           <Link href="/">
             <div>
