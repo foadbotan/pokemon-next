0d5972a5f91d0df01d868b337bb116af95b3414e
Foad Botan <foadbotan@gmail.com>
2022-10-27 11:17:36 +1100

    Create 404 for Pokemon pages

--- pages/[pokemon].js
+++ pages/[pokemon].js
@@ -1,3 +1,4 @@
+import { useRouter } from "next/router";
 import Image from "next/image";
 import Layout from "../components/Layout";
 import { COLORS } from "../constants/colors";
@@ -5,7 +6,19 @@ import { COLORS } from "../constants/colors";
 const URL = "https://pokeapi.co/api/v2/pokemon/";
 
 export default function Pokemon(props) {
-  const { color, name, abilities, types, image, moves, id, stats, japaneseName } = props;
+  const { color, name, abilities, types, image, moves, id, stats, japaneseName, error } = props;
+
+  const router = useRouter();
+  const query = router.query;
+  if (error) {
+    return (
+      <Layout>
+        <h1 className="mb-2 text-center text-4xl">Error 404</h1>
+        <p className="text-center">Pokemon: {query.pokemon}</p>
+        <p className="text-center text-red-500">{error}</p>
+      </Layout>
+    );
+  }
 
   return (
     <Layout
@@ -36,36 +49,45 @@ export default function Pokemon(props) {
   );
 }
 
-export async function getStaticPaths() {
-  const data = await fetch(URL + "?limit=-1").then((res) => res.json());
-  const paths = data.results.map((pokemon) => ({
-    params: { pokemon: pokemon.name },
-  }));
+// export async function getStaticPaths() {
+//   const data = await fetch(URL + "?limit=-1").then((res) => res.json());
+//   const paths = data.results.map((pokemon) => ({
+//     params: { pokemon: pokemon.name },
+//   }));
 
-  return {
-    paths,
-    fallback: false,
-  };
-}
+//   return {
+//     paths,
+//     fallback: false,
+//   };
+// }
 
-export async function getStaticProps({ params: { pokemon } }) {
-  const pokemonData = await fetch(URL + pokemon).then((res) => res.json());
-  const speciesData = await fetch(pokemonData.species.url).then((res) => res.json());
+export async function getServerSideProps({ params: { pokemon } }) {
+  try {
+    const pokemonData = await fetch(URL + pokemon).then((res) => res.json());
+    const speciesData = await fetch(pokemonData.species.url).then((res) => res.json());
 
-  return {
-    props: {
-      japaneseName: speciesData.names.find((name) => name.language.name === "ja").name,
-      color: speciesData.color.name,
-      name: pokemonData.name,
-      abilities: pokemonData.abilities.map(({ ability }) => ability.name),
-      types: pokemonData.types.map(({ type }) => type.name),
-      image: pokemonData.sprites.other["official-artwork"].front_default,
-      moves: pokemonData.moves.map(({ move }) => move.name),
-      id: pokemonData.id.toString(),
-      stats: pokemonData.stats.map(({ base_stat, stat }) => ({
-        base_stat,
-        name: stat.name,
-      })),
-    },
-  };
+    return {
+      props: {
+        japaneseName: speciesData.names.find((name) => name.language.name === "ja").name,
+        color: speciesData.color.name,
+        name: pokemonData.name,
+        abilities: pokemonData.abilities.map(({ ability }) => ability.name),
+        types: pokemonData.types.map(({ type }) => type.name),
+        image: pokemonData.sprites.other["official-artwork"].front_default,
+        moves: pokemonData.moves.map(({ move }) => move.name),
+        id: pokemonData.id.toString(),
+        stats: pokemonData.stats.map(({ base_stat, stat }) => ({
+          base_stat,
+          name: stat.name,
+        })),
+      },
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      props: {
+        error: "Pokemon not found",
+      },
+    };
+  }
 }
