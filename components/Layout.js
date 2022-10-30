624d9e5016d1e041a744fdc4caf4ab5d603242a4
Foad Botan <112809457+foadbotan@users.noreply.github.com>
2022-10-30 17:27:45 +1100

    Responsive redesign (#5)
    * Header fixed position and style
    
    * Reduce card size
    
    * Install react-select
    
    * Add react-select and responsive design
    
    * Style cards: bg color, size, spacing, font color
    
    * New screenshot

--- components/Layout.js
+++ components/Layout.js
@@ -4,25 +4,25 @@ import Link from "next/link";
 
 export default function Layout({ children, title, className = "", style = {} }) {
   return (
-    <div style={style} className={className}>
+    <div>
       <Head>
         <title>{title}</title>
       </Head>
 
-      <main className="container mx-auto">
-        <div className="mb-10 flex items-center justify-center">
-          <Link href="/">
-            <div>
-              <Image
-                src="/pokemon-logo.svg"
-                width="200"
-                height="100"
-                alt="Pokemon logo"
-                className=" cursor-pointer"
-              />
-            </div>
-          </Link>
-        </div>
+      <header className="fixed z-10 flex w-full items-center justify-center bg-white shadow-md">
+        <Link href="/">
+          <div>
+            <Image
+              src="/pokemon-logo.svg"
+              width="200"
+              height="100"
+              alt="Pokemon logo"
+              className=" cursor-pointer"
+            />
+          </div>
+        </Link>
+      </header>
+      <main style={style} className={`pt-32 ${className}`}>
         {children}
       </main>
     </div>
