30cf55129ddbf999b64346256f69f991920e466d
Foad Botan <foadbotan@gmail.com>
2022-11-06 15:10:49 +1100

    Remove Layout

--- pages/[pokemon].js
+++ pages/[pokemon].js
@@ -1,45 +1,49 @@
 import { useRouter } from "next/router";
 import Image from "next/image";
-import Layout from "../components/Layout";
-import { COLORS } from "../constants/colors";
+import Head from "next/head";
 import Error404 from "../components/Error404";
+import { COLORS } from "../constants/colors";
+import { capitalize } from "../utils";
+import Navbar from "../components/Navbar";
 
 const URL = "https://pokeapi.co/api/v2/pokemon/";
 
 export default function Pokemon(props) {
   const { color, name, abilities, types, image, moves, id, stats, japaneseName, error } = props;
+  const backgroundColor = color === "white" ? COLORS[types[0]] : COLORS[color];
+
+  console.log(types);
 
   const router = useRouter();
   const query = router.query;
-
   if (error) return <Error404 error={error} query={query} />;
 
   return (
-    <Layout
-      title={name}
-      style={{
-        backgroundColor: COLORS[color],
-        backgroundImage: `linear-gradient(45deg, transparent 0%, hsl(0 0% 100% / 0.3) 30%, hsl(0 0% 100% / 0.3) 70%, transparent 100%)`,
-      }}
-      className="h-screen"
-    >
-      <p className="absolute text-[12rem] font-black  opacity-10 ">{japaneseName}</p>
-      <div className="flex">
-        <h2 className="text-6xl font-bold capitalize opacity-70">{name}</h2>
-        <Image src={image} alt={name} height="300" width="300" />
+    <div className="min-h-screen" style={{ backgroundColor }}>
+      <Head>
+        <title>{name}</title>
+      </Head>
+      <Navbar name={name} id={id} />
+      <div className="container relative mx-auto">
+        <p className="absolute w-full text-center text-[min(20vw,8rem)] font-black leading-none opacity-70">
+          {japaneseName}
+        </p>
+        <div className="relative mx-auto aspect-square w-2/3 max-w-sm justify-center">
+          <Image src={image} alt={name} layout="fill" />
+        </div>
+        <div className="flex flex-wrap gap-4">
+          {types.map((type) => (
+            <div
+              key={type}
+              className="rounded-full px-4 py-2 text-sm font-bold capitalize"
+              style={{ backgroundColor: COLORS[type] }}
+            >
+              <div className="opacity-60">{type}</div>
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="flex flex-wrap gap-4">
-        {types.map((type) => (
-          <div
-            key={type}
-            className="rounded-full px-4 py-2 text-sm font-bold capitalize"
-            style={{ backgroundColor: COLORS[type] }}
-          >
-            <div className="opacity-60">{type}</div>
-          </div>
-        ))}
-      </div>
-    </Layout>
+    </div>
   );
 }
 
@@ -52,7 +56,7 @@ export async function getServerSideProps({ params: { pokemon } }) {
       props: {
         japaneseName: speciesData.names.find((name) => name.language.name === "ja").name,
         color: speciesData.color.name,
-        name: pokemonData.name,
+        name: capitalize(pokemonData.name),
         abilities: pokemonData.abilities.map(({ ability }) => ability.name),
         types: pokemonData.types.map(({ type }) => type.name),
         image: pokemonData.sprites.other["official-artwork"].front_default,
