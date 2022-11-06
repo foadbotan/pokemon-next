de1c88a65b03636a2d5f49997ddb61b7e802d9e8
Foad Botan <foadbotan@gmail.com>
2022-11-06 17:45:56 +1100

    Style Navbar and image

--- components/Navbar/NavbarHome.js
+++ components/Navbar/NavbarHome.js
@@ -4,7 +4,7 @@ export default function NavbarHome() {
   return (
     <header className="flex items-center justify-center gap-2 py-5">
       <Image src="/pokemon-logo.svg" width="40" height="40" alt="Pokemon logo" />
-      <p className="text-3xl font-black">Pokédex</p>
+      <p className="text-3xl font-black sm:text-5xl">Pokédex</p>
     </header>
   );
 }
