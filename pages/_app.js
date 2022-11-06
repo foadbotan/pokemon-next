6b2c694e9d645695809a96778b47eccae4483045
Foad Botan <foadbotan@gmail.com>
2022-11-06 12:35:14 +1100

    Keep filters on navigation; lift state to _app

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import { useState } from "react";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const [searchFilter, setSearchFilter] = useState("");
+  const [typeFilter, setTypeFilter] = useState([]);
+  const [numberOfPokemonVisible, setNumberOfPokemonVisible] = useState(10);
+
+  const props = {
+    ...pageProps,
+
+    searchFilter,
+    typeFilter,
+    numberOfPokemonVisible,
+
+    setSearchFilter,
+    setTypeFilter,
+    setNumberOfPokemonVisible,
+  };
+
+  return <Component {...props} />;
 }
 
-export default MyApp
+export default MyApp;
