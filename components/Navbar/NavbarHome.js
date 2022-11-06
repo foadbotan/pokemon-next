655b733eb018fafaea642d1e0dcf4e4c1a0e16d9
Foad Botan <foadbotan@gmail.com>
2022-11-06 18:08:24 +1100

    More responsive home navbar

--- components/Navbar/NavbarHome.js
+++ components/Navbar/NavbarHome.js
@@ -3,7 +3,9 @@ import Image from "next/image";
 export default function NavbarHome() {
   return (
     <header className="flex items-center justify-center gap-2 py-5">
-      <Image src="/pokemon-logo.svg" width="40" height="40" alt="Pokemon logo" />
+      <div className="flex w-10 sm:w-16">
+        <Image src="/pokemon-logo.svg" width="100" height="100" alt="Pokemon logo" />
+      </div>
       <p className="text-3xl font-black sm:text-5xl">Pokédex</p>
     </header>
   );
