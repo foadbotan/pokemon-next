291660721056514d9f5164431d16721aa650a678
Foad Botan <foadbotan@gmail.com>
2022-11-06 22:48:48 +1100

    Space new logo

--- components/Navbar/NavbarHome.js
+++ components/Navbar/NavbarHome.js
@@ -2,11 +2,8 @@ import Image from "next/image";
 
 export default function NavbarHome() {
   return (
-    <header className="flex items-center justify-center gap-2 py-5">
-      <div className="flex w-10 sm:w-16">
-        <Image src="/pokemon-logo.svg" width="100" height="100" alt="Pokemon logo" />
-      </div>
-      <p className="text-3xl font-black sm:text-5xl">Pokédex</p>
+    <header className="mx-auto flex w-[calc(20vw+10rem)] justify-center p-5 sm:p-10">
+      <Image src="/pokemon-logo.svg" width="300" height="50" alt="Pokemon logo" />
     </header>
   );
 }
