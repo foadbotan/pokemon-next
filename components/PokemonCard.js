d4f35b5dc5b51fbd595c03872237938602f5b8a3
Foad Botan <foadbotan@gmail.com>
2022-11-07 14:34:28 +1100

    Lighten card bg

--- components/PokemonCard.js
+++ components/PokemonCard.js
@@ -9,22 +9,20 @@ function Pokemon({ name, id, url, types, image }) {
   return (
     <Link href={`/${name}`}>
       <div
-        className="relative flex aspect-square w-28 cursor-pointer flex-col overflow-hidden rounded-xl border-2 bg-white transition hover:scale-105 sm:w-36 lg:w-44"
+        className="relative flex aspect-square w-32 cursor-pointer flex-col overflow-hidden rounded-xl border-2 bg-white transition hover:scale-105 sm:w-40"
         style={{ borderColor: color }}
       >
-        <p className="absolute right-2 top-1 text-xs" style={{ color }}>
-          #{`${id}`.padStart(3, "0")}
-        </p>
         <div
-          className="absolute top-0 right-0 aspect-square w-60 rounded-full sm:w-96"
+          className="absolute bottom-0 left-0 aspect-square w-60 rounded-full opacity-40 sm:w-96"
           style={{ backgroundColor: color }}
         ></div>
+        <p className="absolute right-2 top-1 text-xs text-white">#{`${id}`.padStart(3, "0")}</p>
         <div className="p-5">
           <Image src={image} width="200" height="200" alt={name} />
         </div>
         <h3
-          className="absolute bottom-0 left-0 w-full p-2 text-xs font-bold capitalize text-white sm:text-sm lg:text-base"
-          style={{ textShadow: `0 0 2px ${color}` }}
+          className="absolute bottom-0 left-0 w-full p-2 text-xs font-bold capitalize  sm:text-sm lg:text-base"
+          style={{ color }}
         >
           {name}
         </h3>
