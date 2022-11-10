50e4c30e18591e8a309b5a419e3f441bbd8619bc
Foad Botan <foadbotan@gmail.com>
2022-11-10 21:44:24 +1100

    Minor styling

--- components/StatsBar.js
+++ components/StatsBar.js
@@ -3,17 +3,14 @@ export default function StatsBar({ stat }) {
 
   return (
     <div>
-      <div className="flex justify-between py-1 text-sm font-medium leading-none text-white">
+      <div className="flex justify-between py-1 text-sm font-medium leading-none">
         <span className="uppercase">{stat.name}</span>
         <span>
           {stat.base_stat} <span className="text-[8px]"> / 255</span>
         </span>
       </div>
       <div className="w-full rounded-full bg-[hsla(0,0%,100%,0.3)]">
-        <div
-          className="h-2.5 rounded-full bg-white opacity-70"
-          style={{ width: `${percent}%` }}
-        ></div>
+        <div className="h-2.5 rounded-full bg-white" style={{ width: `${percent}%` }}></div>
       </div>
     </div>
   );
