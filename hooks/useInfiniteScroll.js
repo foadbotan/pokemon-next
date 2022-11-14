b3df4909d442d22bc9946354b9f24ae8c92cea4f
Foad Botan <foadbotan@gmail.com>
2022-11-15 02:37:10 +1100

    Fix: Infinite scrolling on desktop

--- hooks/useInfiniteScroll.js
+++ hooks/useInfiniteScroll.js
@@ -4,9 +4,14 @@ export default function useInfiniteScroll(callback) {
   const ref = useRef();
 
   useEffect(() => {
-    const observer = new IntersectionObserver(([entry]) => {
-      if (entry.isIntersecting) callback();
-    });
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) callback();
+      },
+      {
+        rootMargin: "1px",
+      }
+    );
 
     if (ref.current) observer.observe(ref.current);
 
