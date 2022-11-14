be6b3e35917b0468775050e6d057e79f52cf6128
Foad Botan <foadbotan@gmail.com>
2022-11-15 00:41:06 +1100

    Create type select dropdown

--- components/TypeButton.js
+++ components/TypeButton.js
@@ -5,7 +5,7 @@ export default function TypeButton({ onClick, type, label, className }) {
   return (
     <button
       onClick={onClick}
-      className={`flex cursor-pointer items-center gap-2 rounded-2xl border border-white text-center text-xs font-medium uppercase text-white hover:brightness-90 ${className}`}
+      className={`flex cursor-pointer items-center justify-between gap-2 rounded-2xl border border-white text-center text-xs font-medium uppercase text-white hover:brightness-90 ${className}`}
       style={{ backgroundColor: COLORS[type] }}
     >
       {label && label}
