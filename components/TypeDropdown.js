4fedc7f507431240ad1b9785f5981a9bb88929c7
Foad Botan <foadbotan@gmail.com>
2022-11-26 15:46:56 +1100

    Refactor: remove DropdownColumn component

--- components/TypeDropdown.js
+++ components/TypeDropdown.js
@@ -1,13 +1,16 @@
-import DropdownColumn from "./DropdownColumn";
+import TypeButton from "./TypeButton";
 
 export default function TypeDropdown({ allTypes, setTypeFilter }) {
-  const half = allTypes.length / 2;
-  const columnOne = allTypes.slice(0, half);
-  const columnTwo = allTypes.slice(half);
+  function addType(type) {
+    setTypeFilter((prevTypes) => {
+      return prevTypes.includes(type) ? prevTypes : [...prevTypes, type];
+    });
+  }
   return (
-    <div className="absolute top-full z-10 mt-2 flex gap-2 rounded-3xl border bg-white p-4 shadow-xl sm:right-0">
-      <DropdownColumn types={columnOne} setTypeFilter={setTypeFilter} />
-      <DropdownColumn types={columnTwo} setTypeFilter={setTypeFilter} />
+    <div className="absolute top-full z-10 mt-2 grid w-72 grid-cols-2 gap-2 rounded-3xl border bg-white p-4 shadow-xl sm:right-0">
+      {allTypes.map((type) => (
+        <TypeButton key={type} type={type} className="py-1 px-3" onClick={() => addType(type)} />
+      ))}
     </div>
   );
 }
