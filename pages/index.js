c628b193fda4e6714aa16d830435ff3945da6d45
Foad Botan <foadbotan@gmail.com>
2023-06-01 04:34:08 +1000

    Replace flex with grid on Pokemon cards container

--- pages/index.js
+++ pages/index.js
@@ -21,9 +21,13 @@ export default function Home({ allPokemon, allTypes, error, store }) {
   const filteredPokemon = allPokemon
     .filter(({ name, id }) => {
       const searchTerms = state.searchFilter.toLowerCase().trim().split(" ");
-      return searchTerms.every((search) => name.includes(search) || id.includes(search));
+      return searchTerms.every(
+        search => name.includes(search) || id.includes(search)
+      );
     })
-    .filter((pokemon) => state.typeFilter.every((type) => pokemon.types.includes(type)));
+    .filter(pokemon =>
+      state.typeFilter.every(type => pokemon.types.includes(type))
+    );
 
   function displayMorePokemon() {
     if (state.numberOfPokemonVisible > filteredPokemon.length) return;
@@ -35,7 +39,7 @@ export default function Home({ allPokemon, allTypes, error, store }) {
   }
 
   return (
-    <div className="min-h-screen bg-neutral-100">
+    <div className="min-h-screen bg-neutral-200">
       <Head>
         <title>Pokedex</title>
         <meta
@@ -45,10 +49,15 @@ export default function Home({ allPokemon, allTypes, error, store }) {
         <link rel="icon" href="/pokeball.svg" />
       </Head>
       <header className="mx-auto w-48 py-5 sm:w-80 sm:py-10">
-        <Image src="/pokemon-logo.svg" width="300" height="50" alt="Pokemon logo" />
+        <Image
+          src="/pokemon-logo.svg"
+          width="300"
+          height="50"
+          alt="Pokemon logo"
+        />
       </header>
-      <main className="container mx-auto flex flex-col gap-4">
-        <div className="mx-auto flex w-full max-w-2xl flex-col justify-center gap-2 sm:my-5 sm:flex-row">
+      <main className="container flex flex-col items-center gap-4">
+        <div className="flex w-full max-w-2xl flex-col justify-center gap-2 sm:my-5 sm:flex-row">
           <Search store={store} />
           <div className="flex items-center gap-2">
             <TypeSelect allTypes={allTypes} store={store} />
@@ -62,14 +71,16 @@ export default function Home({ allPokemon, allTypes, error, store }) {
           </div>
         </div>
 
-        <section className="flex w-full flex-wrap justify-center gap-2 lg:gap-4">
-          {filteredPokemon.slice(0, state.numberOfPokemonVisible).map((pokemon) => (
-            <PokemonCard key={pokemon.name} {...pokemon} />
-          ))}
+        <section className="grid grid-cols-2 gap-6 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5">
+          {filteredPokemon
+            .slice(0, state.numberOfPokemonVisible)
+            .map(pokemon => (
+              <PokemonCard key={pokemon.name} {...pokemon} />
+            ))}
         </section>
 
         {filteredPokemon.length === 0 && (
-          <h1 className="text-center text-2xl font-bold">No Pokemon found</h1>
+          <h1 className="text-2xl font-bold">No Pokemon found</h1>
         )}
 
         <div ref={infiniteScrollRef}></div>
@@ -80,12 +91,16 @@ export default function Home({ allPokemon, allTypes, error, store }) {
 
 export async function getStaticProps() {
   try {
-    const { results: typesData } = await fetch(TYPES_URL).then((res) => res.json());
-    const { results: pokemonData } = await fetch(POKEMON_URL).then((res) => res.json());
+    const { results: typesData } = await fetch(TYPES_URL).then(res =>
+      res.json()
+    );
+    const { results: pokemonData } = await fetch(POKEMON_URL).then(res =>
+      res.json()
+    );
 
     const allTypes = await Promise.all(
-      typesData.map(async (type) => {
-        const { pokemon } = await fetch(type.url).then((res) => res.json());
+      typesData.map(async type => {
+        const { pokemon } = await fetch(type.url).then(res => res.json());
         return {
           type: type.name,
           pokemonList: pokemon.map(({ pokemon }) => pokemon.name),
@@ -93,7 +108,7 @@ export async function getStaticProps() {
       })
     );
 
-    const allPokemon = pokemonData.map((pokemon) => {
+    const allPokemon = pokemonData.map(pokemon => {
       return {
         name: pokemon.name,
         id: pokemon.url.split("/")[6],
@@ -108,7 +123,7 @@ export async function getStaticProps() {
         allPokemon,
         allTypes: allTypes
           .map(({ type }) => type)
-          .filter((type) => type !== "unknown" && type !== "shadow"), // remove unused pokemon types "shadow" and "unknown"
+          .filter(type => type !== "unknown" && type !== "shadow"), // remove unused pokemon types "shadow" and "unknown"
       },
     };
   } catch (error) {
